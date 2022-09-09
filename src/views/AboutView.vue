6dcd03f225f188a0c178ef65e15ca946e614b148
p_zjielliu <liuzhengjie@yuewen.com>
2022-09-09 16:51:21 +0800

    feat: AboutView demo

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Hover the number to see animation</p>
+    <div class="counter"></div>
   </div>
 </template>
 
@@ -9,7 +11,30 @@
   .about {
     min-height: 100vh;
     display: flex;
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
   }
+}
+
+@property --num {
+  syntax: "<integer>";
+  initial-value: 0;
+  inherits: false;
+}
+
+.counter {
+  transition: --num 5s;
+  counter-set: num var(--num);
+  font: 800 40px system-ui;
+  margin-top: 30px;
+}
+
+.counter:hover {
+  --num: 100;
+}
+
+.counter::after {
+  content: counter(num);
 }
 </style>
